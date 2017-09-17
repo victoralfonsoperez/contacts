b9c837d16b702f025f8e67e99e1ff2f1e0f0d8b8
victor perez <vaperezr@unal..edu.co>
2017-09-17 10:43:40 -0500

    Update the contactList component and displays the contact info

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -5,7 +5,19 @@ class ListContacts extends Component {
     return (
       <ol className="contact-list">
         {this.props.contacts.map(contact => (
-          <li key={contact.id}>{contact.name}</li>
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}
+            />
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button className="contact-remove">Remove</button>
+          </li>
         ))}
       </ol>
     );
