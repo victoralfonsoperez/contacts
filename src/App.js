b43ef1bf627ab6cb965738381cf6c941ff13f253
victor perez <vaperezr@unal..edu.co>
2017-09-17 10:36:36 -0500

    Creates the ContactList component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
 const contacts = [
   {
@@ -23,7 +24,7 @@ const contacts = [
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return <ListContacts contacts={contacts} />;
   }
 }
 
