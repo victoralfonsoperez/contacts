5e1272602bcbd37a49d34ee207d378fc7142c9d3
victor perez <vaperezr@unal..edu.co>
2017-09-17 10:25:26 -0500

    Update folder structure and starter files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
-
-  return <ol>
-    {people.map(person => (
-      <li key={person.name}>{person.name}</li>
-    ))}
-    </ol>
+const contacts = [
+  {
+    id: 'ryan',
+    name: 'Ryan Florence',
+    email: 'ryan@reacttraining.com',
+    avatarURL: 'http://localhost:5001/ryan.jpg'
+  },
+  {
+    id: 'michael',
+    name: 'Michael Jackson',
+    email: 'michael@reacttraining.com',
+    avatarURL: 'http://localhost:5001/michael.jpg'
+  },
+  {
+    id: 'tyler',
+    name: 'Tyler McGinnis',
+    email: 'tyler@reacttraining.com',
+    avatarURL: 'http://localhost:5001/tyler.jpg'
   }
-}
+];
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Michael' },
-          { name: 'Ryan' },
-          { name: 'Tyler' }
-        ]}/>
-
-        <ContactList contacts={[
-          { name: 'Amanda' },
-          { name: 'Richard' },
-          { name: 'Geoff' }
-        ]}/>
-      </div>
-    );
+    return <div>Hello World</div>;
   }
 }
 
